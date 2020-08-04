067ebb447e91977c3835ff834e9533f43a7d8cc7
shankarsmartgeek <pesalashankar48@gmail.com>
2020-08-04 14:30:13 +0530

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,41 @@
+class PostsController < ApplicationController
+    http_basic_authenticate_with name: "shankar", password: "12345", except: [:index, :show]
+    def index
+        @posts=Post.all
+    end
+    def new
+        @post = Post.new
+    end
+    def show
+        @post=Post.find(params[:id])
+    end
+    def create
+        #render plain: params[:post].inspect
+        @post = Post.new(post_params)
+        if(@post.save)
+            redirect_to @post
+        else
+            render 'new'
+        end
+    end
+    def update
+        @post=Post.find(params[:id])
+        if(@post.update(post_params))
+            redirect_to @post
+        else
+            render 'edit'
+        end
+
+    end
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to posts_path  
+    end
+    def edit
+        @post=Post.find(params[:id])
+    end
+    private def post_params
+        params.require(:post).permit(:title,:body)
+    end
+end
